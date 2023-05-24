d31dbac97fabe1f271b571d5e56c8cc5b27f5aa1
zo-el <joelulahanna@gmail.com>
2023-05-23 20:01:08 -0500

    hpos_hc_connect: update comments to pass test

--- crates/hpos_hc_connect/src/lib.rs
+++ crates/hpos_hc_connect/src/lib.rs
@@ -6,24 +6,27 @@
 //!
 //! ### Expected Environment vars
 //! ```
-//! HOLOCHAIN_DEFAULT_PASSWORD=<password to unlock holochain conductor>
-//! CORE_HAPP_FILE=<path to a config.json file used for the configure-holochain service>
-//! DEV_UID_OVERRIDE=<network-seed that is used to create new hash spaces with different holo-nixpkgs builds>
-//! LAIR_CONNECTION_URL=<string uri to lcoation of lair keystore> *OPTIONAL*
-//! HOLOCHAIN_WORKING_DIR=<path to holochains working dir> *OPTIONAL is LAIR_CONNECTION_URL is not provided*
+//! // HOLOCHAIN_DEFAULT_PASSWORD=<password to unlock holochain conductor>
+//! // CORE_HAPP_FILE=<path to a config.json file used for the configure-holochain service>
+//! // DEV_UID_OVERRIDE=<network-seed that is used to create new hash spaces with different holo-nixpkgs builds>
+//! // LAIR_CONNECTION_URL=<string uri to lcoation of lair keystore> *OPTIONAL*
+//! // HOLOCHAIN_WORKING_DIR=<path to holochains working dir> *OPTIONAL is LAIR_CONNECTION_URL is not provided*
 //! ```
 //! ### Example:
 //!
 //! ```rust
 //! use hpos_hc_connect::HolofuelAgent;
-//! let mut agent: HolofuelAgent = HolofuelAgent::connect().await?;
-//! let result: ExternIO = agent
-//!     .zome_call(
-//!         ZomeName::from("transactor"),
-//!         FunctionName::from("get_ledger"),
-//!         ExternIO::encode(())?,
-//!     )
-//!     .await?;
+//! use holochain_types::prelude::{ExternIO, FunctionName, ZomeName};
+//! pub async fn test() {
+//!     let mut agent: HolofuelAgent = HolofuelAgent::connect().await.unwrap();
+//!     let result: ExternIO = agent
+//!         .zome_call(
+//!             ZomeName::from("transactor"),
+//!             FunctionName::from("get_ledger"),
+//!             ExternIO::encode(()).unwrap(),
+//!          )
+//!          .await.unwrap();
+//! }
 //! ```
 
 pub mod hf_agent;
